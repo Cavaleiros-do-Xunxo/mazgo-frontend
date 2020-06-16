09de3a937b2f51ce9274ddbbd282e327dbc1c61e
Tiago Valdrich <tiago.valdrich@hotmail.com>
2020-06-15 23:41:24 -0300

    Example of xunxo

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import Product from '../models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  private REST_API_URL = "https://mazgo.notfab.net";
+
   constructor(private httpClient: HttpClient) { }
 
-  // @TODO: fetch data from API.
-  getProducts(): Array<Product> {
-    const apple = new Product('Apple', 15, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvh_fUPArPDkJj9dWXPFV3iWrmVaL8mYhYdUQB3QCqRnA72a99&usqp=CAU');
-    const potato = new Product('Potato', 0, 'https://www.veggieprezi.com/wp-content/uploads/2017/09/20170731_214129-555x688.jpg');
-    const bread = new Product('Bread', 5, 'https://www.ambitiouskitchen.com/wp-content/uploads/2019/04/Multi-Grain-Seedy-Sandwich-Bread-Edited-5sq.jpg');
-
-    return [apple, potato, bread];
+  async getProducts(): Promise<any> {
+    try {
+      const data = await this.httpClient.get(this.REST_API_URL + "/products").toPromise();
+      return data;
+    } catch (err) {
+      console.error(`Error fetching data from API: ${err}`);
+      return [];
+    }
   }
 }
--- src/app/views/products/products.component.ts
+++ src/app/views/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { ProductService } from 'src/app/services/product.service';
-import Product from 'src/app/models/Product';
 
 @Component({
   selector: 'app-products',
@@ -11,14 +10,18 @@ import Product from 'src/app/models/Product';
 })
 export class ProductsComponent implements OnInit {
 
-  public stockStatus: FormControl = new FormControl('');
-  public productName: FormControl = new FormControl('');
-  public products: Product[];
-  
+  public productName: FormControl = new FormControl('', [Validators.min(1)]);
+  public products: any = [];
+
   public STOCK_STATUS_ENUM = {
     IN_STOCK: 'IN_STOCK',
     OUT_OF_STOCK: 'OUT_OF_STOCK'
   };
+  
+  public statusCheckbox = {
+    IN_STOCK: false,
+    OUT_OF_STOCK: false,
+  }
 
   constructor(private productService: ProductService) { }
 
@@ -26,8 +29,44 @@ export class ProductsComponent implements OnInit {
     this.getProducts();
   }
 
+  private hasSomethingChecked(): boolean {
+    return this.statusCheckbox.OUT_OF_STOCK || this.statusCheckbox.IN_STOCK;
+  }
+
+  // Xunxeira because we are lazy to create this route properly on back-end
   public getProducts(): void {
-    this.products = this.productService.getProducts();
+    this.productService.getProducts().then(data => {
+      let _products = [];
+
+      if (
+        this.hasSomethingChecked() && 
+        this.productName.valid && 
+        Array.isArray(data.items)
+      ) {
+        if (this.statusCheckbox.IN_STOCK) {
+          _products.push(...(data.items.filter(product => product.quantity > 0)));
+        }
+  
+        if (this.statusCheckbox.OUT_OF_STOCK) {
+          _products.push(...(data.items.filter(product => product.quantity === 0)));
+        }
+
+        if (this.productName.valid && _products.length > 0) {
+          _products = _products.filter(product => product.name.indexOf(this.productName.value) >= 0);
+        }
+      } else {
+        if (this.productName.valid) {
+          _products = data.items.filter(product => product.name.indexOf(this.productName.value) >= 0);
+        } else {
+          _products = data.items;
+        }
+      }
+
+      this.products = _products;
+    }).catch((err) => {
+      console.error(`Error fetching products: ${err}`);
+      this.products = [];
+    });
   }
 
   public getStockStatus(quantityOfItems: number): string {
@@ -42,7 +81,7 @@ export class ProductsComponent implements OnInit {
     if (quantityOfItems >= 10) {
       return "success";
     } else if (quantityOfItems > 0 && quantityOfItems < 10) {
-      return "warning"
+      return "warning";
     } else {
       return "danger";
     }
